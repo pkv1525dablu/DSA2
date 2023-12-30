eb486cec0183520bd5d32548bd19e59247b6c222
pkv1525dablu <pkv1525dablu@gmail.com>
2023-12-30 18:45:50 +0530

    commite 34

--- src/dsa22/SSS.java
+++ src/dsa22/SSS.java
@@ -3,6 +3,9 @@ package dsa22;
 public class SSS {
 	public static void main(String[] args) {
 		System.out.println("jdncjdsjcsdc");
+		System.out.println("jdncjdsjcsdc");
+		System.out.println("jdncjdsjcsdc");
+		System.out.println("jdncjdsjcsdc");
 	}
 
 }
