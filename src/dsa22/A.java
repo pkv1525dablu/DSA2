7d40e6fc9db655cf821b83bae726cc04488497d9
aks06 <aks06@DESKTOP-3MROQB7>
2023-12-30 10:11:02 +0530

    commit2 3

--- src/dsa22/A.java
+++ src/dsa22/A.java
@@ -5,5 +5,10 @@ public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("hello 2");
 	System.out.println("hello 24");
+	System.out.println("hello 24");
+	System.out.println("hello 24");
+	System.out.println("hello 24");
+	System.out.println("hello 24");
+	System.out.println("hello 24");
 }
 }
