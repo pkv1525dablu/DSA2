9d2471fca226b57ba786bf4cd4aa8dc68a792733
pkv1525dablu <110728895+pkv1525dablu@users.noreply.github.com>
2023-12-30 14:01:50 +0530

    Update A.java

--- src/dsa22/A.java
+++ src/dsa22/A.java
@@ -5,9 +5,7 @@ public static void main(String[] args) {
 	System.out.println("Hello World");
 	System.out.println("hello 2");
 	System.out.println("hello 24");
-	System.out.println("hello 24");
-	System.out.println("hello 24");
-	System.out.println("hello 24");
+
 	System.out.println("hello 24");
 	System.out.println("hello 24");
 }
