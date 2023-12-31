dcfd0d953090a2ea7402129f785db20ba711d02d
pkv1525dablu <110728895+pkv1525dablu@users.noreply.github.com>
2023-12-31 15:25:24 +0530

    Update SSS.java
    new chnage

--- src/dsa22/SSS.java
+++ src/dsa22/SSS.java
@@ -4,8 +4,6 @@ public class SSS {
 	public static void main(String[] args) {
 		System.out.println("jdncjdsjcsdc");
 		System.out.println("jdncjdsjcsdc");
-		System.out.println("jdncjdsjcsdc");
-		System.out.println("jdncjdsjcsdc");
 	}
 
 }
